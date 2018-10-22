b92d6472e22bba35e2b975b5cbac07b77e86818e
ErickChamorro <eschamorro97@misena.edu.co>
2018-10-22 16:22:49 -0500

    se hizo la preuba de acceder al value del item selecionado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  public eficiencia:string;
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   }
+
+  calificar(){
+    const alert = this.alertCtrl.create({
+      title: 'Confirmacion',
+      message: 'estado de eficiencia: '+ this.eficiencia,
+      buttons: ['listo','okey no']
+    });
+    alert.present();
+  }
+
+
 
 }
