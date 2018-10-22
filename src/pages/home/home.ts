7fd81089159ce28412f933c74b7b81f16bbb52c3
ErickChamorro <eschamorro97@misena.edu.co>
2018-10-22 17:49:59 -0500

    prototipo casi terminado, solo falta las observaciones

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,17 +7,147 @@ import { NavController, AlertController } from 'ionic-angular';
 })
 export class HomePage {
 
-  public eficiencia:string;
+  public eficiencia: string;
+  public amor: string;
   constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   }
 
   calificar(){
-    const alert = this.alertCtrl.create({
-      title: 'Confirmacion',
-      message: 'estado de eficiencia: '+ this.eficiencia,
-      buttons: ['listo','okey no']
-    });
-    alert.present();
+    if(this.eficiencia == null){
+      const alert = this.alertCtrl.create({
+        title: 'Un favor',
+        message: 'seleccione un item primero',
+        buttons: ['okey', 'y si no quiero?']
+      });
+      alert.present();
+    }else{
+      if(this.eficiencia == "malo"){
+        const bloque = document.getElementById("bloque-eficiencia");
+        bloque.removeAttribute("hidden");
+        bloque.className = "show";
+        bloque.style.backgroundColor = "crimson";
+        bloque.style.color = "white";
+        bloque.style.borderColor = "white";
+        const estado = this.eficiencia;
+        bloque.textContent = "estado de eficiencia: " + estado;
+    
+        const radios = document.getElementById("contenedor-formulario-uno");
+        radios.setAttribute("hidden", "");
+      }if(this.eficiencia == "excelente"){
+        const bloque = document.getElementById("bloque-eficiencia");
+        bloque.removeAttribute("hidden");
+        bloque.className = "show";
+        bloque.style.backgroundColor = "chartreuse";
+        bloque.style.color = "green";
+        bloque.style.borderColor = "green";
+        const estado = this.eficiencia;
+        bloque.textContent = "estado de eficiencia: " + estado;
+    
+        const radios = document.getElementById("contenedor-formulario-uno");
+        radios.setAttribute("hidden", "");
+      }if(this.eficiencia == "regular"){
+        const bloque = document.getElementById("bloque-eficiencia");
+        bloque.removeAttribute("hidden");
+        bloque.className = "show";
+        bloque.style.backgroundColor = "orange";
+        bloque.style.color = "black";
+        bloque.style.borderColor = "black";
+        const estado = this.eficiencia;
+        bloque.textContent = "estado de eficiencia: " + estado;
+    
+        const radios = document.getElementById("contenedor-formulario-uno");
+        radios.setAttribute("hidden", "");
+      }if(this.eficiencia == "bueno"){
+        const bloque = document.getElementById("bloque-eficiencia");
+        bloque.removeAttribute("hidden");
+        bloque.className = "show";
+        bloque.style.backgroundColor = "cyan";
+        bloque.style.color = "white";
+        bloque.style.borderColor = "white";
+        const estado = this.eficiencia;
+        bloque.textContent = "estado de eficiencia: " + estado;
+    
+        const radios = document.getElementById("contenedor-formulario-uno");
+        radios.setAttribute("hidden", "");
+      }
+    }
+  }
+
+  retornar(){
+    const bloque = document.getElementById("bloque-eficiencia");
+    const radios = document.getElementById("contenedor-formulario-uno");
+    bloque.setAttribute("hidden", "");
+    radios.removeAttribute("hidden");
+    radios.className = "show";
+  }
+
+  juzgue(){
+    if(this.amor == null){
+      const alert = this.alertCtrl.create({
+        title: 'Un favor',
+        message: 'seleccione un item primero',
+        buttons: ['okey', 'y si no quiero?']
+      });
+      alert.present();
+    }else{
+      if(this.amor == "despreciado"){
+        const bloque = document.getElementById("bloque-amor");
+        bloque.removeAttribute("hidden");
+        bloque.className = "show";
+        bloque.style.backgroundColor = "crimson";
+        bloque.style.color = "white";
+        bloque.style.borderColor = "white";
+        const estado = this.amor;
+        bloque.textContent = "estado de amor: " + estado;
+    
+        const radios = document.getElementById("contenedor-formulario-dos");
+        radios.setAttribute("hidden", "");
+      }if(this.amor == "positivo"){
+        const bloque = document.getElementById("bloque-amor");
+        bloque.removeAttribute("hidden");
+        bloque.className = "show";
+        bloque.style.backgroundColor = "chartreuse";
+        bloque.style.color = "green";
+        bloque.style.borderColor = "green";
+        const estado = this.amor;
+        bloque.textContent = "estado de amor: " + estado;
+    
+        const radios = document.getElementById("contenedor-formulario-dos");
+        radios.setAttribute("hidden", "");
+      }if(this.amor == "no"){
+        const bloque = document.getElementById("bloque-amor");
+        bloque.removeAttribute("hidden");
+        bloque.className = "show";
+        bloque.style.backgroundColor = "orange";
+        bloque.style.color = "black";
+        bloque.style.borderColor = "black";
+        const estado = this.amor;
+        bloque.textContent = "estado de amor: " + estado;
+    
+        const radios = document.getElementById("contenedor-formulario-dos");
+        radios.setAttribute("hidden", "");
+      }if(this.amor == "creo"){
+        const bloque = document.getElementById("bloque-amor");
+        bloque.removeAttribute("hidden");
+        bloque.className = "show";
+        bloque.style.backgroundColor = "cyan";
+        bloque.style.color = "white";
+        bloque.style.borderColor = "white";
+        const estado = this.amor;
+        bloque.textContent = "estado de amor: " + estado;
+    
+        const radios = document.getElementById("contenedor-formulario-dos");
+        radios.setAttribute("hidden", "");
+      }
+    }
+  }
+
+  retornar_amor(){
+    const bloque = document.getElementById("bloque-amor");
+    const radios = document.getElementById("contenedor-formulario-dos");
+    bloque.setAttribute("hidden", "");
+    radios.removeAttribute("hidden");
+    radios.className = "show";
   }
 
 
